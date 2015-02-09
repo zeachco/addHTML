8ba4a07c614acdc64146b80511d08944ae4ee636
Olivier Rousseau-Guyot <zeachco@gmail.com>
2015-02-09 12:29:50 -0500

    removed inline, added find feature and incrementation

--- addHTML.js
+++ addHTML.js
@@ -1,6 +1,17 @@
-function addHTML(tag) {
-  var e = document.createElement(tag.split(/[#\.]gi/)[0]);
-  e.appendTo = function(p) { return (typeof p === 'object' ? p : document.createElement(p)).appendChild(e) && e; };
+function addHTML(t) {
+  var s = addHTML;
+  function sel(d, p){
+    p = p || document;
+    if(typeof d === 'object'){ return s(d); }
+    switch(d[0]){
+      case '': return s(d);
+      case '#': return s(p.getElementById(d));
+      case '.': return s(p.getElementsByClass(d)[0]);
+      default: return s(p.getElementsByTagName(d)[0]);
+    }
+  }
+  var e = typeof t === 'object' ? t : document.createElement(t.split(/[#\.]gi/)[0]);
+  e.appendTo = function(p) { return s(p).appendChild(e) && e; };
   e.attr = function(k, v) { return typeof v === 'undefined' ? e.getAttribute(k) : e.setAttribute(k, v) || e; };
   e.html = function(h) { return h ? (e.innerHTML = h) && e : e.innerHTML; };
   e.css = function(k, v) { return typeof v === 'undefined' ? e.style[k] : (e.style[k] = v) + '1' && e; };
@@ -9,5 +20,6 @@ function addHTML(tag) {
   e.removeClass = function(c) { return (e.className = e.className.replace(c, '').trim()) + '1' && e; };
   e.remove = function() { e.parentNode.removeChild(e); };
   e.delay = function(d, cb) { setTimeout(function() { cb.call(e); }, d); return e; };
+  e.find = function(d){ return sel(d, e); };
   return e;
 }
